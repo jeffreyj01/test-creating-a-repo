09f0a41831f1d17f566901db1e9cac26badd77bf
Jeff Johnson <jeffreyj0112@gmail.com>
2020-12-31 15:08:48 -0700

    first commit

---
+++ ClassLibrary1/MainClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldLibrary
+{
+    public class MainClass
+    {
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using HelloWorldLibrary;
+using System;
+
+namespace HelloWorldConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainClass hello = new MainClass();
+            Console.WriteLine(hello.HelloWorld());
+            Console.ReadLine();
+        }
+    }
+}
